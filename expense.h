f60c724f30f4c64076c35bd584c002e0cc31078a
Burak Nebioglu <buraknebioglu@Buraks-Air.home>
2021-12-07 02:12:00 +0100

    Added Discretionary class

--- expense.h
+++ expense.h
@@ -71,3 +71,16 @@ public:
         cout << "Start : " << start_month << " " << how_many_months << endl;
     }
 };
+
+class Discretionary : public Expense
+{
+private:
+    /* data */
+
+public:
+    Discretionary(int start_month, int how_many_months, int month, float price, string definition)
+        : Expense(month, price, definition)
+    {
+        cout << "Start : " << start_month << " " << how_many_months << endl;
+    }
+};
