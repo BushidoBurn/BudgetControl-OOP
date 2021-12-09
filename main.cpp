2be7acb6edbaeff4f8f777bdf35b00d9fbf358b8
Burak Nebioglu <buraknebioglu@Buraks-Air.home>
2021-12-09 21:56:36 +0100

    made cleaning on main

--- main.cpp
+++ main.cpp
@@ -10,26 +10,12 @@ using std::endl;
 int main()
 {
 
-    cout << "Testing .gitignore" << endl;
-
-    // Expense exp = Expense(1, 11.3, "Nike bag");
-    // exp.showExpenseDetails();
-    // exp.showTotalNumberOfExpenses();
-
-    FixedExpense f_exp = FixedExpense(1, 3, 1, 12.3, "Rent");
+     FixedExpense f_exp = FixedExpense(1, 3, 1, 12.3, "Rent");
     FixedExpense d_exp = FixedExpense(1, 3, 1, 12.3, "Rent1");
 
     Discretionary a = Discretionary(1, 3, 1, 12.5, "Rent22");
     Discretionary b = Discretionary(1, 3, 1, 12.7, "Rent33");
 
-    // Expense * exp_p;
-
-    // for (Expense *ex : f_exp.getAllExpenses())
-    // {
-    //     ex->print_detail(ex);
-    //     if (ex->findExpense("Rent33", ex))
-    //         cout << "Great found " << ex->findExpense("Rent33", ex)->getDefinition() << " " << ex->findExpense("Rent33", ex)->getPrice() << " " << ex->findExpense("Rent33", ex)->getMonth();
-    // }
     for (Expense *e : f_exp.findExpenseInVector("Rent33"))
     {
         cout << e->getDefinition() << endl;
